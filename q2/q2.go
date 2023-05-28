6295c1b665e8c5d606b5f8e185bf83b1e6f816c9
João Gabriel <126472751+joaodelabio@users.noreply.github.com>
2023-05-27 21:49:54 -0300

    q2.go

--- q2/q2.go
+++ q2/q2.go
@@ -20,43 +20,6 @@ type Employee struct {
 }
 
 func CalculateTotalSalary(employee *Employee) (float64, error) {
-
-	totalBonus := 0.0
-	for _, bonus := range emp.MonthlyBonus {
-		totalBonus += bonus
-	}
-
-	totalSalary := emp.BaseSalary + totalBonus
-
-	if totalBonus > 1500.0 {
-		emp.Position = "Senior " + emp.Position
-	}
-
-	return totalSalary, nil
-}
-
-func main() {
-	employee := &Employee{
-		ID:         1,
-		Name:       "John Doe",
-		Position:   "Developer",
-		BaseSalary: 5000.0,
-		MonthlyBonus: []float64{
-			1000.0,
-			800.0,
-			750.0,
-		},
-	}
-
-	totalSalary, err := calculateTotalSalary(employee)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-
-	fmt.Println("Total Salary:", totalSalary)
-	fmt.Println("Position:", employee.Position)
-}
-
-	return 1, errors.New("Not implemented yet")
+	// Seu código aqui
+	return 0, errors.New("Not implemented yet")
 }
